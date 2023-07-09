162056bbc0f3fde671804c22188f67550e62595f
Ganaa <ganaa.12396@yahoo.com>
2023-07-10 00:01:57 +0900

    ganaa

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import Carousel from "@/components/Carousel/Carousel";
-import allProducts from "@/components/allProducts/allProducts";
 export default function Home() {
 
   return (
