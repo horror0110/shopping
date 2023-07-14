22f623f7a1ec7c949601f2ad295c66bf3f240d49
Ganaa <ganaa.12396@yahoo.com>
2023-07-14 20:54:45 +0900

    last

--- components/Coat/Coat.js
+++ components/Coat/Coat.js
@@ -1,47 +1,39 @@
-
-"use client";
-import React, { useState, useEffect, useRef, useContext } from "react";
-const thousandify = require("thousandify");
-import Link from "next/link";
+import React, { useState, useEffect, useRef , useContext } from 'react';
+import thousandify from 'thousandify';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
-import { ThemeContext } from "@/context/ThemeContext";
-import Addcart from "@/components/AddCart/AddCart";
-import Spinner from "@/components/Loading/Loading";
-import Coat from "@/components/Coat/Coat";
+import { ThemeContext } from '@/context/ThemeContext';
 
-const Eregtey = () => {
+
+const Coat = () => {
   const [products, setProducts] = useState([]);
   const productsContainerRef = useRef(null);
   const { data: session, status: sessionStatus } = useSession();
   const router = useRouter();
-  const {handleAddToCart , setSpinner} = useContext(ThemeContext);
+  const {handleAddToCart} = useContext(ThemeContext);
 
   useEffect(() => {
-    setSpinner(true);
-    fetch("api/categories/64b019c1d329d9bf552a3bd1/products", {
-      method: "GET",
+    fetch('api/categories/64b1338c5d72684c77576f3e/products', {
+      method: 'GET',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
     })
       .then((res) => res.json())
       .then((data) => {
-        setSpinner(false);
         setProducts(data);
       })
       .catch((error) => {
-        console.error("Error:", error);
+        console.error('Error:', error);
       });
   }, []);
 
   const handleNextClick = () => {
-    
     const container = productsContainerRef.current;
-    container.scrollBy({ left: container.offsetWidth, behavior: "smooth" });
+    container.scrollBy({ left: container.offsetWidth, behavior: 'smooth' });
   };
+
   const moveOrder = (el) => {
-    setSpinner(true);
    
 
     if (sessionStatus === 'unauthenticated') {
@@ -67,25 +59,21 @@ const Eregtey = () => {
       })
         .then((response) => {
           if (response.ok) {
-            setSpinner(false);
             handleAddToCart();
-            
           } else {
             throw new Error('Failed to add order');
           }
         })
         .catch((error) => {
-          setSpinner(false);
           console.log(error);
         });
     }
   };
+
   return (
     <div>
-      <Spinner/>
-      <Addcart/>
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-2xl font-bold mb-4">Эрэгтэй хувцас</h1>
+        <h1 className="text-2xl font-bold mb-4">Куртик</h1>
         <div className="overflow-x-auto" ref={productsContainerRef}>
           <div className="flex space-x-4">
             {products.map((product, index) => (
@@ -97,58 +85,30 @@ const Eregtey = () => {
                   <img
                     src={product.photo[0]}
                     alt={product.name}
-                    className="h-40 w-full object-cover mb-4"
+                    className="h-40 object-cover mb-4"
                   />
                   <h2 className="text-lg font-bold mb-2">{product.name}</h2>
-                  <p className="text-sm text-gray-500 mb-2 w-[200px]">
-                    {product.description}
-                  </p>
+                  <p className="text-sm text-gray-500 mb-2">{product.description}</p>
                   <p className="text-sm text-gray-500 mb-2">
-                    Үнэ:
-                    <strong className="text-black">
-                      {thousandify(product.price)}₮
-                    </strong>{" "}
+                    Үнэ: <strong className="text-black">{thousandify(product.price)}₮</strong>
                   </p>
-                  <p className="text-sm text-gray-500 mb-2">
-                    Бэлэн байгаа:{" "}
-                    <strong className="text-black">{product.balance}</strong>
+                  <p className="text-sm text-gray-500 mb-2 ">
+                    Бэлэн байгаа: <strong className="text-black">{product.balance}</strong>
                   </p>
-                  <div className="flex space-x-2">
-                    {product.color.map((color) => (
-                      <div
-                        key={color}
-                        className={`w-6 h-6 rounded-full bg-${color} border border-gray-300`}
-                        style={{ backgroundColor: color }}
-                      ></div>
-                    ))}
-                  </div>
                 </div>
-                <div>
-                  <div className="flex space-x-2">
-                    {product.size.map((size) => (
-                      <div
-                        key={size}
-                        className="border border-gray-300 rounded p-1"
-                      >
-                        {size}
-                      </div>
-                    ))}
-                  </div>
-                  <div className="flex flex-col ">
+                <div className="flex flex-col">
                   <button
-                    onClick={()=>moveOrder(product)}
+                    onClick={() => moveOrder(product)}
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mt-4 w-[150px]"
                   >
                     Сагсанд хийх
                   </button>
-
-                    <Link
-                      href={product._id}
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mt-4 w-[150px]"
-                    >
-                      Дэлгэрэнгүй
-                    </Link>
-                  </div>
+                  <a
+                    href={product._id}
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded mt-4 w-[150px]"
+                  >
+                    Дэлгэрэнгүй
+                  </a>
                 </div>
               </div>
             ))}
@@ -161,9 +121,8 @@ const Eregtey = () => {
       >
         Next
       </button>
-      <Coat/>
     </div>
   );
 };
 
-export default Eregtey;
+export default Coat;
